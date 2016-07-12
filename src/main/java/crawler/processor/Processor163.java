0d897b855783511890cd8598908bc6f39215b0a3
= <=>
2016-07-12 14:00:34 +0800

    完成163爬虫

--- src/main/java/crawler/processor/Processor163.java
+++ src/main/java/crawler/processor/Processor163.java
@@ -8,49 +8,43 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.apache.commons.lang3.StringUtils;
-
-
-
-
-
-
-
-
-
-
 import crawler.model.Tags;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Request;
 import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.selector.JsonPathSelector;
 
-public class Processor163 {
+public class Processor163 implements Processor{
 	private static String INIT_URL = "http://news.163.com/special/0001220O/news_json.js";
-	static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+	static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");	
 	
-	public static void processor(Page page){
+	public void processor(Page page){
 		if(page.getUrl().toString().equals(INIT_URL))
 			initProcessor(page);
-		if(timeJudgement(page))
+		else
 			contentProcessor(page);
 	}
 	
-	public static void init(Spider spider){
-		spider.addUrl(INIT_URL);
-}
+	public void init(Spider spider){
+		spider.addRequest(new Request(INIT_URL).putExtra("_charset", "gb2312"));
+	}
 	
 	private static void initProcessor(Page page) {
 		List<String> linkList = new JsonPathSelector("$.news[*]").selectList(page.getRawText().substring(page.getRawText().indexOf("{")));
-		for(String links : linkList){
-			page.addTargetRequests(new JsonPathSelector("$[*].l").selectList(links));
-		}	
+		List<String> tags = new JsonPathSelector("$.category[*].n").selectList(page.getRawText().substring(page.getRawText().indexOf("{")));
+		for(int i = 0;i< linkList.size();i++){
+			List<String> link = new JsonPathSelector("$[*].l").selectList(linkList.get(i));
+			List<String> title = new JsonPathSelector("$[*].t").selectList(linkList.get(i));
+			List<String> time = new JsonPathSelector("$[*].p").selectList(linkList.get(i));
+			String tag = tags.get(i);
+			for(int j = 0;j< link.size();j++){
+				if(strToDate(time.get(j)).after(today()))
+					page.addTargetRequest(new Request(link.get(j)).putExtra("title", title.get(j)).putExtra("time", strToDate(time.get(j))).putExtra("tag", tag));
+			}
+		}
 	}
 	
-	private static boolean timeJudgement(Page page){
+	private static Date today(){
 		String strTime = sdf.format(new Date());
 		strTime = strTime.substring(0,10);
 		strTime = strTime + " 00:00:00";
@@ -61,63 +55,30 @@ public class Processor163 {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}            //今天零点的时间
-		
-		String newsTime = page.getUrl().toString();
-		Date newsDateTime = new Date();
-		if(newsTime!=null){
-			Pattern timePattern = Pattern.compile("http://\\w+.163.com/(\\d+)/(\\d{2})(\\d{2})/(\\d+)/\\w+");
-			Matcher m = timePattern.matcher(newsTime);
-			if(m.find()){
-				try {
-					newsDateTime = sdf.parse("20"+m.group(1)+"-"+m.group(2)+"-"+m.group(3)+" "+m.group(4)+":00:00");
-				} catch (ParseException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		}
-		if(newsDateTime.after(today)) 
-			return true;
-		else 
-			return false;
+		return today;
 	}
 	
-	private static void contentProcessor(Page page) {
-		String title = page.getHtml().xpath("//h1/text()").toString();
-		List<String> subContent = page.getHtml().xpath("//div[@class='post_text']/p/text()").all();
-		String content = "";
-		content = StringUtils.join(subContent.toArray(),"<br/>");
-		String pulishTime = page.getHtml().xpath("//div[@class='post_time_source']/text()").toString();
-		pulishTime = pulishTime.trim().substring(0,19);
-		SimpleDateFormat timeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		Date newsDatetime = new Date();
+	private static Date strToDate(String str){
+		Date newsTime = new Date();
 		try {
-			newsDatetime = timeFormat.parse(pulishTime);
-		} catch (ParseException e) {
+			newsTime = sdf.parse(str);
+		} catch (ParseException e1) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			e1.printStackTrace();
 		}
-			
-		List<Tags> tags = new ArrayList<Tags>();	//tags存放所有标签
-		Tags tmpTags = new Tags("测试父tag",0);		//父标签第二个参数0
-		tags.add(tmpTags);
-			
-		tmpTags = new Tags("测试子tag",1);				//子标签第二个参数1
-		tags.add(tmpTags);
-			
-		tmpTags = new Tags("测试关键字",-1);			//关键字类标签第二个参数 -1
-		tags.add(tmpTags);
-			
-			
-		page.putField("title",title);
-		page.putField("subtitle","");
-		page.putField("content",content);
-		page.putField("link",page.getUrl().toString());
-		page.putField("newsTime", newsDatetime);
-		page.putField("tags", tags);		
+		return newsTime;
 	}
-
 	
-
-
+	private static void contentProcessor(Page page) {				
+		page.putField("title",page.getRequest().getExtra("title").toString());
+		page.putField("subtitle","");
+		page.putField("content",page.getHtml().smartContent());
+		page.putField("link",page.getUrl().toString());
+		page.putField("newsTime", page.getRequest().getExtra("time"));
+		List<Tags> tags = new ArrayList<Tags>();	//tags存放所有标签
+		Tags tmpTags = new Tags(page.getRequest().getExtra("tag").toString(),0);		//父标签第二个参数0
+		tags.add(tmpTags);
+		page.putField("tags", tags);
+		
+	}
 }

164f1037dbe191e803a5a2da317a3db63b2afe5f
Kootain <gaoty@qq.com>
2016-07-14 16:27:56 +0800

    Change 修改tag插入逻辑,完善logger

--- src/main/java/crawler/pipeline/MysqlPipeLine.java
+++ src/main/java/crawler/pipeline/MysqlPipeLine.java
@@ -9,7 +9,10 @@ import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
+import javax.swing.text.html.HTML.Tag;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -30,21 +33,26 @@ import crawler.model.Tags;
 @Scope("prototype")
 public class MysqlPipeLine implements Pipeline{
 	
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
 	@Autowired
 	private NewsDao newsDao;
 
 	@Override
 	public void process(ResultItems resultItems, Task task){
-//		News news = (News)resultItems.get("itemObject");
 		News news = genNews(resultItems);
+		if(news == null){
+			return;
+		}
 		List<Tags> tags = resultItems.get("tags");
 		int parentId = 0;
-		Tags tmpTags = null;
 		try {
 			newsDao.addNews(news);
+			logger.info("插入新闻:"+news.getLink());
 			if(null!=tags){
-				for(Tags tag:tags){
-					tmpTags = newsDao.findTag(tag.getTagName());
+				for(int i=0;i<tags.size();i++){
+					Tags tag = tags.get(i);
+					Tags tmpTags = newsDao.findTag(tag.getTagName());
 					if(tmpTags==null){//新标签
 						if(tag.getTagType()==-1){	//如果不是关键字标签,处理父子关系
 							tag.setParentTag(-1);
@@ -52,38 +60,52 @@ public class MysqlPipeLine implements Pipeline{
 							tag.setParentTag(parentId);
 						}
 						newsDao.addTags(tag);
-						if(tag.getTagType()==0){	//默认传入第一条是父标签,如果是父标签,则余下非关键字标签父id设为第一个标签插入后返回的id
-							parentId = tag.getTid();
-						}	
+						logger.info("插入Tag:"+tag.getTagName());
+					}else{
+						tags.set(i, tmpTags);
+					}
+					if(tag.getTagType()==0){	//默认传入第一条是父标签,如果是父标签,则余下非关键字标签父id设为第一个标签插入后返回的id
+						parentId = tag.getTid();
 					}
 				}
 			}
 		} catch (DuplicateKeyException e) {
-			System.err.println("重复数据");
+			logger.warn("重复数据!");
 		} catch (MySQLDataException e) {
-			e.printStackTrace();
+			logger.error("新建标签失败!");
+		} finally{
+			if(news.getId()!=0){
+				for(Tags tag:tags){
+					Map<String, Integer> map = new HashMap<String, Integer>();	//插入新闻和tag的关系
+					map.put("tid", tag.getTid());
+					map.put("nid", news.getId());
+					try {
+						newsDao.addT2N(map);
+						logger.info(String.format("建立新闻标签关系: news[%d] - tag[%d]", map.get("nid"),map.get("tid")));
+					} catch (MySQLDataException e) {
+						logger.error("新闻标签关系建立失败");
+					}
+				}
+			}
 		}
-		Map<String, Integer> map = new HashMap<String, Integer>();	//插入新闻和tag的关系
-		map.put("tid", tmpTags.getTid());
-		map.put("nid", news.getId());
-		try {
-			newsDao.addT2N(map);
-		} catch (MySQLDataException e) {
-			System.err.println("新建标签失败");
-		}	
 
 	}
 	
 	private News genNews(ResultItems resultItems){
-    	News news = new News();
-    	news.setTitle(resultItems.get("title").toString());
-    	news.setSubtitle(resultItems.get("subtitle").toString());
-    	news.setContent(resultItems.get("content").toString());
-    	news.setResource(resultItems.get("resource").toString());
-    	news.setLink(resultItems.get("link").toString());
-    	news.setNewsTime((Date)resultItems.get("newsTime"));
-    	news.setCreateTime(new Date());
-    	news.setIshidden(false);
+		News news = null;
+		try{
+			news = new News();
+			news.setTitle(resultItems.get("title").toString());
+			news.setSubtitle(resultItems.get("subtitle").toString());
+			news.setContent(resultItems.get("content").toString());
+			news.setResource(resultItems.get("resource").toString());
+			news.setLink(resultItems.get("link").toString());
+			news.setNewsTime((Date)resultItems.get("newsTime"));
+			news.setCreateTime(new Date());
+			news.setIshidden(false);
+		} catch (NullPointerException e){
+			logger.error("新闻初始化错误，Null!");
+		}
     	return news;
     }
 }
